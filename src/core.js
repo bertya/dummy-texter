8e4e8d26d57ef24b144ea88d5e48c4cf39746c4e
Bing <bing@shiros-MacBook-Pro.local>
2017-12-26 10:35:59 -0500

    Init commit

---
+++ src/core.js
@@ -0,0 +1,12 @@
+export default class dummyTexter {
+  
+    constructor() {
+      this.test = 1;
+    }
+  
+    get() {
+      console.log(`result is ${this.test}`);
+    }
+  
+  }
+  
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Tool from './core.js';
+
+let tool = new Tool();
+
+tool.get();
